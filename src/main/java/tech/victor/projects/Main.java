dbc804307eae54a7145f1e95dbe73a2da1300c9e
Victor <victorhenriquebl11@gmail.com>
2024-12-06 02:56:56 -0300

    Reusing instantiation

--- src/main/java/tech/victor/projects/Main.java
+++ src/main/java/tech/victor/projects/Main.java
@@ -7,6 +7,7 @@ import tech.victor.projects.model.entities.Seller;
 public class Main {
     public static void main(String[] args) {
         SellerDAO sellerDAO = DAOFactory.createSellerDAO();
+        System.out.println("==== TEST 1: seller findById ====");
         Seller seller = sellerDAO.findById(3);
         System.out.println(seller);
     }
--- src/main/java/tech/victor/projects/model/dao/impl/SellerDAOimplJDBC.java
+++ src/main/java/tech/victor/projects/model/dao/impl/SellerDAOimplJDBC.java
@@ -48,16 +48,8 @@ public class SellerDAOimplJDBC implements SellerDAO {
             st.setInt(1, id);
             rs = st.executeQuery();
             if(rs.next()) {
-                Department dep = new Department();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("DepName"));
-                Seller seller = new Seller();
-                seller.setId(rs.getInt("Id"));
-                seller.setName(rs.getString("Name"));
-                seller.setEmail(rs.getString("Email"));
-                seller.setBaseSalary(rs.getDouble("BaseSalary"));
-                seller.setBirthDate(rs.getDate("BirthDate").toLocalDate());
-                seller.setDepartment(dep);
+                Department dep = instantiateDepartment(rs);
+                Seller seller = instantiateSeller(rs, dep);
                 return seller;
             }
             return null;
@@ -67,6 +59,24 @@ public class SellerDAOimplJDBC implements SellerDAO {
             DB.closeStatement(st);
             DB.closeResultSet(rs);
         }
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller seller = new Seller();
+        seller.setId(rs.getInt("Id"));
+        seller.setName(rs.getString("Name"));
+        seller.setEmail(rs.getString("Email"));
+        seller.setBaseSalary(rs.getDouble("BaseSalary"));
+        seller.setBirthDate(rs.getDate("BirthDate").toLocalDate());
+        seller.setDepartment(dep);
+        return seller;
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+        return dep;
     }
 
     @Override
