55be14145e72f8ffe6af325b1417ede8fbf63b71
Victor <victorhenriquebl11@gmail.com>
2024-12-06 16:22:39 -0300

    refactor: update class DAOFactory

--- src/main/java/tech/victor/projects/model/dao/DAOFactory.java
+++ src/main/java/tech/victor/projects/model/dao/DAOFactory.java
@@ -1,11 +1,16 @@
 package tech.victor.projects.model.dao;
 
 import tech.victor.projects.db.DB;
+import tech.victor.projects.model.dao.impl.DepartmentDAOimplJDBC;
 import tech.victor.projects.model.dao.impl.SellerDAOimplJDBC;
 
 public class DAOFactory {
 
     public static SellerDAO createSellerDAO() {
         return new SellerDAOimplJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDAO createDepartmentDAO() {
+        return new DepartmentDAOimplJDBC(DB.getConnection());
     }
 }
